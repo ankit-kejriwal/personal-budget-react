bb1e5fe3cc93f46e81e8bcae4a5388c999258d08
ankit <ankitkejriwaal@gmail.com>
2020-10-13 04:31:15 -0400

    moved the personal budget in react

---
+++ src/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer id="footer-nav" aria-label="Footer" className="bottom">
+        <div className="center">
+            All rights reserved &copy; Fabio Nolasco
+        </div>
+    </footer>
+  );
+}
+
+export default Footer;
